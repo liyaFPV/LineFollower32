0b99ebca8fa6e29a547f313f9673dcad0da6c83a
liyaFPV <lbkmzc942@gmail.com>
2026-03-09 21:16:36 +0500

    изменён тип регулятора

--- src/config.h
+++ src/config.h
@@ -2,14 +2,6 @@
 
 #include <Arduino.h>
 
-/* ===== WIFI CONFIG ===== */
-#define WIFI_SSID "Keenetic"
-#define WIFI_PASSWORD "VPN2011VPN"
-
-#define AP_SSID "LineFollower32"
-#define AP_PASS "VPN2011VPN"
-
-/* ===== SENSOR PINOUT ===== */
 #define s0 36
 #define s1 39
 #define s2 34
@@ -27,17 +19,5 @@
 #define butnStart 0
 
 /*=====PID PARAMETERS=====*/
-#define BaseSpeed 100
-#define MaxSpeed 200
-#define updateTime 1
-const int SE[] = {0,1,2,3,4,5,6,7};
-const int speed[][2] = {
-    {0,255},//lms rms
-    {150,255},
-    {150,255},
-    {255,255},
-    {255,255},
-    {255,150},
-    {255,100},
-    {255,0}
-};
+#define BaseSpeed 77
+#define sensitivity 3000
--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,8 @@
 #include <Arduino.h>
 #include "config.h"
-#include <WiFi.h>
-#include "web.h"
-#include "core1.h"
-#include <freertos/task.h>
-#include <uPID.h>
-
-uPID pid(D_INPUT | I_SATURATE);
-
-float Kp = 0.0;
-float Ki = 0.0;
-float Kd = 0.0;
-int sensitivity = 0;
-int sensorAverage = 0;
-int sensitivityOffset = 30;
 bool robotStarted = false;
-
 int LMS,RMS,Speed=0;
 
-int getE(){
-    int sensorV[8]={analogRead(s0),analogRead(s1),analogRead(s2),analogRead(s3),analogRead(s4),analogRead(s5),analogRead(s6),analogRead(s7)};
-    for(int i=0;i<8;i++){
-        if(sensorV[i]>sensorAverage+sensitivityOffset){
-            return SE[i];
-        }
-    }
-    return -1;
-}
-
-void connectWiFi() {
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-
-    Serial.print("Connecting WiFi");
-
-    int timeout = 20;
-
-    while (WiFi.status() != WL_CONNECTED && timeout--) {
-        delay(500);
-        Serial.print(".");
-    }
-
-    if (WiFi.status() != WL_CONNECTED) {
-        Serial.println("\nWiFi fail → AP Mode");
-
-        WiFi.mode(WIFI_AP);
-        WiFi.softAP(AP_SSID, AP_PASS);
-
-        Serial.print("AP IP: ");
-        Serial.println(WiFi.softAPIP());
-    } else {
-        Serial.println("\nWiFi connected");
-        Serial.println(WiFi.localIP());
-    }
-}
 
 void pinSetup() {
     pinMode(s0, INPUT);
@@ -70,63 +19,59 @@ void pinSetup() {
     pinMode(motorR, OUTPUT);
 }
 
-void selfTest() {
-    analogWrite(motorL, 128);
-    analogWrite(motorR, 128);
-    delay(500);
+void setup() {
+    Serial.begin(9600);
+    pinSetup();
     analogWrite(motorL, 0);
     analogWrite(motorR, 0);
+}
 
-    int x = analogRead(s0) + analogRead(s1) + analogRead(s2) + analogRead(s3) + analogRead(s4) + analogRead(s5) + analogRead(s6) + analogRead(s7);
-    x = x / 8;
-    sensorAverage = x;
-
-    Serial.println(Kp);
-    Serial.println(Ki);
-    Serial.println(Kd);
-    Serial.println(sensitivity);
-
-    if (x == 0) {
-        Serial.println("Self test Failed");
-    } else {
-        Serial.println("Self test Passed");
-        return;
+void race(){
+    if(analogRead(s0)>=sensitivity){
+        Speed=255;
+        LMS=constrain(BaseSpeed+Speed,0,255);
+        RMS=constrain(BaseSpeed-Speed,0,255);
+    }
+    if(analogRead(s1)>=sensitivity){
+        Speed=200;
+        LMS=constrain(BaseSpeed+Speed,0,255);
+        RMS=constrain(BaseSpeed-Speed,0,255);
+    }
+    if(analogRead(s2)>=sensitivity){
+        Speed=127;
+        LMS=constrain(BaseSpeed+Speed,0,255);
+        RMS=constrain(BaseSpeed-Speed,0,255);
+    }
+    if(analogRead(s3)>=sensitivity){
+        Speed=0;
+        LMS=constrain(BaseSpeed+Speed,0,255);
+        RMS=constrain(BaseSpeed-Speed,0,255);
+    }
+    if(analogRead(s4)>=sensitivity){
+        Speed=0;
+        LMS=constrain(BaseSpeed+Speed,0,255);
+        RMS=constrain(BaseSpeed-Speed,0,255);
+    }
+    if(analogRead(s5)>=sensitivity){
+        Speed=127;
+        LMS=constrain(BaseSpeed+Speed,0,255);
+        RMS=constrain(BaseSpeed-Speed,0,255);
+    }
+    if(analogRead(s6)>=sensitivity){
+        Speed=200;
+        LMS=constrain(BaseSpeed+Speed,0,255);
+        RMS=constrain(BaseSpeed-Speed,0,255);
+    }
+    if(analogRead(s7)>=sensitivity){
+        Speed=255;
+        LMS=constrain(BaseSpeed+Speed,0,255);
+        RMS=constrain(BaseSpeed-Speed,0,255);
     }
 }
 
-void setup() {
-    Serial.begin(9600);
-    delay(500);
-    pid.setDt(updateTime);
-    pid.outMax = 255;
-    pid.outMin = -255;
-    
-    Serial.println("\n\n=== LineFollower32 Starting ===");
-    Serial.print("Setup running on core: ");
-    Serial.println(xPortGetCoreID());
-    
-    connectWiFi();
-    beginWebServer();
-    pinSetup();
-    //selfTest();
-    analogWrite(motorL, 0);
-    analogWrite(motorR, 0);
-    initCore1Task();
-}
 
 void loop() {
-    pid.setKp(10);
-    pid.setKi(20);
-    pid.setKd(5);
     if (robotStarted) {
-        if(digitalRead(butnStart) == LOW) {
-            robotStarted = false;
-        }
-        int E = getE();
-        LMS = speed[E][0];
-        RMS = speed[E][1];
-        analogWrite(motorL, LMS);
-        analogWrite(motorR, RMS);
-        delay(updateTime);
+        
     }
 }
